d46dd808904dad6cb81be339f9a57dbbf28c78ce
stephaniedewit <stephanie.dewit@student.hu.nl>
2022-05-22 01:43:24 +0200

    Eerste functie, sw_tidy, eindelijk werkend gekregen.

--- R/sw_tidy.R
+++ R/sw_tidy.R
@@ -1,3 +1,19 @@
-sw_tidy <- function(tidy_tibble, group, column) {
-  tidy_tibble %>% group_by(group) %>% summarise(p.value.sw = shapiro.test(column))$p.value
+#' Title
+#'
+#' @param data A tidy tibble with group and values column
+#' @param groep The groups measured
+#' @param kolom The measurement values
+#'
+#' @return A P-value
+#' @importFrom stats shapiro.test
+#' @importFrom magrittr %>%
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @export
+#'
+#' @examples
+#' rabbit_ears <- data.frame(rabbitnr = seq(1,20),colour = c(rep("beige",10),rep("brown",10)), earlength = c(17.5,13.0,18.6,19.4,20.9,18.9,22.8,20.3,22.1,20.4,23.0,24.1,26.1,25.2,27.8,28.5,26.5,21.5,20.2,18.8))
+#' sw_tidy(rabbit_ears, colour, earlength)
+sw_tidy <- function(data, groep, kolom) {
+  data %>% group_by({{ groep }}) %>% summarise(p.value.sw = shapiro.test({{ kolom }})$p.value)
 }
