b80678ac77d3e7ae0f1ea95ac8401df250a94359
stephaniedewit <stephanie.dewit@student.hu.nl>
2022-05-22 03:10:08 +0200

    Derde functie, cor_scatterplot, toegevoegd en werkend gemaakt.

---
+++ R/cor_scatterplot.R
@@ -0,0 +1,30 @@
+#' Title
+#'
+#' @param data A tidy tibble with grouping and two value columns.
+#' @param kolom_x The measurement values of a variable which are to correlate to...
+#' @param kolom_y ...the measurement values of another variable
+#' @param groep The groups into which we can divide the values
+#'
+#' @return A scatterplot to visualize the correlation between twe two variables, with a Pearson's correlation coefficient
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 annotate
+#' @importFrom stats cor.test
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @export
+#'
+#' @examples
+#' library("palmerpenguins")
+#' cor_scatterplot(penguins, penguins$body_mass_g, penguins$flipper_length_mm, penguins$species)
+cor_scatterplot <- function(data, kolom_x, kolom_y, groep) {
+
+cor_coefficient<-round(cor.test(kolom_x, kolom_y, method = c("pearson"))$estimate, 2)
+
+ggplot(data = penguins, aes(x = kolom_x, y = kolom_y)) +
+  geom_point(aes(color = groep), size = 1, alpha = 0.8) +
+  annotate("text",x = (min(kolom_x, na.rm = TRUE) + 1000), y = max(kolom_y, na.rm = TRUE), size = 4, label = paste("Pearson's r = ", cor_coefficient), colour = "red")
+}
+
--- R/gem_barchart.R
+++ R/gem_barchart.R
@@ -2,8 +2,8 @@
 #'
 #'
 #' @param data A tidy tibble with group and values column
-#' @param groep The groups measured
-#' @param kolom The measurement values
+#' @param groep The column with the measured groups
+#' @param waarde The column with measurement values
 #'
 #' @return A barchart with for each param groep the mean param kolom with error bars of standard deviation 1.
 #' @importFrom magrittr %>%
@@ -19,8 +19,8 @@
 #' @examples
 #' rabbit_ears <- data.frame(rabbitnr = seq(1,20),colour = c(rep("beige",10),rep("brown",10)), earlength = c(17.5,13.0,18.6,19.4,20.9,18.9,22.8,20.3,22.1,20.4,23.0,24.1,26.1,25.2,27.8,28.5,26.5,21.5,20.2,18.8))
 #' gem_barchart(rabbit_ears, colour, earlength)
-gem_barchart <- function(data, groep, kolom) {
-  summary <- data %>% group_by({{ groep }}) %>% summarise(gem = mean({{ kolom }}), stdev = sd({{ kolom }}))
+gem_barchart <- function(data, groep, waarde) {
+  summary <- data %>% group_by({{ groep }}) %>% summarise(gem = mean({{ waarde }}), stdev = sd({{ waarde }}))
   ggplot(summary, aes(x = {{ groep }}, y = gem, fill = {{ groep }})) +
      geom_col() +
      geom_errorbar(aes(ymin = gem - stdev, ymax = gem + stdev), width = 0.2)
