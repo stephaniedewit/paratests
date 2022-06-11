e43dde4244814a1739d14a13188f5314d786afb4
stephaniedewit <stephanie.dewit@student.hu.nl>
2022-06-11 16:51:14 +0200

    Functions aangepast (Engels, theme_bw()) en vignette aangepast (cor_scatterplot() voorbeeld laat nu een verband zien i.p.v. dat er geen verband is)

--- R/sw_tidy.R
+++ R/sw_tidy.R
@@ -1,8 +1,8 @@
-#' Title
+#' Shapiro Wilk normality test for tidy data
 #'
 #' @param data A tidy tibble with group and values column
-#' @param groep The column with the measured groups
-#' @param waarde The column with measurement values
+#' @param group The column with the measured groups
+#' @param value The column with measurement values
 #'
 #' @return A P-value
 #' @importFrom stats shapiro.test
@@ -14,6 +14,6 @@
 #' @examples
 #' rabbit_ears <- data.frame(rabbitnr = seq(1,20),colour = c(rep("beige",10),rep("brown",10)), earlength = c(17.5,13.0,18.6,19.4,20.9,18.9,22.8,20.3,22.1,20.4,23.0,24.1,26.1,25.2,27.8,28.5,26.5,21.5,20.2,18.8))
 #' sw_tidy(rabbit_ears, colour, earlength)
-sw_tidy <- function(data, groep, waarde) {
-  data %>% group_by({{ groep }}) %>% summarise(p.value.sw = shapiro.test({{ waarde }})$p.value)
+sw_tidy <- function(data, group, value) {
+  data %>% group_by({{ group }}) %>% summarise(p.value.sw = shapiro.test({{ value }})$p.value)
 }
